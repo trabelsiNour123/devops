d6c1cc6519a7134d801d5a9b5d9f25a1677dde93
nour trabelsi <144689582+trabelsiNour123@users.noreply.github.com>
2025-12-02 17:30:54 +0100

    first commit

---
+++ src/main/java/tn/esprit/avecmaven/AvecmavenApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.avecmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvecmavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvecmavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/avecmaven/AvecmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.avecmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvecmavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
